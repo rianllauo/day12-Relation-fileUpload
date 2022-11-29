346692a6fce761644705279ccdb7fc7e98905232
rianllauo <rianllauo@gmail.com>
2022-11-29 14:38:46 +0700

    create CRUD from database

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"myproject-page/connection"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -33,22 +34,17 @@ func main() {
 }
 
 type Project struct {
-	Id          int
-	Title       string
-	DateStart   string
-	DateEnd     string
-	Description string
+	ID                     int
+	Title                  string
+	DateStart              time.Time
+	DateEnd                time.Time
+	Format_date_start      string
+	Format_date_start_edit string
+	Format_date_end        string
+	Description            string
 }
 
-var projects = []Project{
-	{
-		Title:       "Aplikasi web dumbways",
-		DateStart:   "11 november 2022",
-		DateEnd:     "12 desember 2022",
-		Description: "lorem ipsum dolor si amet",
-		// NodeJs:        "public/img/nodejs.svg",
-	},
-}
+// var projects = []Project{}
 
 func addProject(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
@@ -57,28 +53,25 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	// title := r.PostForm.Get("title")
-	// content := r.PostForm.Get("content")
-	// dateStart := r.PostForm.Get("date-start")
-	// dateEnd := r.PostForm.Get("date-end")
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	dateStart := r.PostForm.Get("date-start")
+	dateEnd := r.PostForm.Get("date-end")
 
-	// var newProject = Project{
-	// 	Title:       title,
-	// 	Description: content,
-	// 	DateStart:   dateStart,
-	// 	DateEnd:     dateEnd,
-	// }
-
-	dataProject, errQuery := connection.Conn.Query(context.Background(), `INSERT INTO public.tb_projects(
-		id, title, description)
-		VALUES ( 'title coba', 'lorem ipsum dolor si amet' )`)
+	_, errQuery := connection.Conn.Exec(context.Background(), "INSERT INTO public.tb_projects(title, start_date, end_date, description) VALUES ($1, $2, $3, $4)", title, dateStart, dateEnd, content)
 	if errQuery != nil {
 		fmt.Println("Message : " + errQuery.Error())
 		return
 	}
+
+	// var newProject = Project{
+	// 	DateStartEdit: dateStart,
+	// 	DateEndEdit: dateEnd,
+	// }
+
 	// projects = append(projects, newProject)
 
-	fmt.Println(dataProject)
+	fmt.Println(dateStart)
 
 	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
@@ -96,19 +89,15 @@ func formEditProject(w http.ResponseWriter, r *http.Request) {
 
 	var ProjectEdit = Project{}
 
-	for i, data := range projects {
-		if i == index {
-			ProjectEdit = Project{
-				Id:          i,
-				Title:       data.Title,
-				Description: data.Description,
-				DateStart:   data.DateStart,
-				DateEnd:     data.DateEnd,
-			}
-		}
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, title, start_date, end_date, description FROM tb_projects WHERE id = $1", index).Scan(&ProjectEdit.ID, &ProjectEdit.Title, &ProjectEdit.DateStart, &ProjectEdit.DateEnd, &ProjectEdit.Description)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
 	}
 
-	// fmt.Println(projects)
+	ProjectEdit.Format_date_start = ProjectEdit.DateStart.Format("2 January 2006")
+	ProjectEdit.Format_date_end = ProjectEdit.DateEnd.Format("2 January 2006")
 
 	dataEdit := map[string]interface{}{
 		"Project": ProjectEdit,
@@ -131,17 +120,24 @@ func editProject(w http.ResponseWriter, r *http.Request) {
 	dateStart := r.PostForm.Get("date-start")
 	dateEnd := r.PostForm.Get("date-end")
 
-	var newProject = Project{
-		Title:       title,
-		Description: content,
-		DateStart:   dateStart,
-		DateEnd:     dateEnd,
+	_, errQuery := connection.Conn.Exec(context.Background(),
+		"UPDATE public.tb_projects SET title=$1, start_date=$2, end_date=$3, description=$4 WHERE id = $5", title, dateStart, dateEnd, content, index)
+	if errQuery != nil {
+		fmt.Println("Message : " + errQuery.Error())
+		return
 	}
 
-	// projects = append(projects, newProject)
-	projects[index] = newProject
+	// var newProject = Project{
+	// 	Title:       title,
+	// 	Description: content,
+	// 	DateStart:   dateStart,
+	// 	DateEnd:     dateEnd,
+	// }
 
-	fmt.Println(index)
+	// // projects = append(projects, newProject)
+	// projects[index] = newProject
+
+	// fmt.Println(index)
 	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
@@ -154,7 +150,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	dataProject, errQuery := connection.Conn.Query(context.Background(), "SELECT id, title, description FROM tb_projects")
+	dataProject, errQuery := connection.Conn.Query(context.Background(), "SELECT id, title, start_date, end_date, description FROM tb_projects")
 	if errQuery != nil {
 		fmt.Println("Message : " + errQuery.Error())
 		return
@@ -165,12 +161,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	for dataProject.Next() {
 		var each = Project{}
 
-		err := dataProject.Scan(&each.Id, &each.Title, &each.Description)
+		err := dataProject.Scan(&each.ID, &each.Title, &each.DateStart, &each.DateEnd, &each.Description)
 		if err != nil {
 			fmt.Println("Message : " + err.Error())
 			return
 		}
 
+		each.Format_date_start = each.DateStart.Format("2 January 2006")
+		each.Format_date_end = each.DateEnd.Format("2 January 2006")
 		result = append(result, each)
 	}
 
@@ -210,16 +208,15 @@ func projectDetail(w http.ResponseWriter, r *http.Request) {
 
 	var ProjectDetail = Project{}
 
-	for index, data := range projects {
-		if index == id {
-			ProjectDetail = Project{
-				Title:       data.Title,
-				Description: data.Description,
-				DateStart:   data.DateStart,
-				DateEnd:     data.DateEnd,
-			}
-		}
+	err = connection.Conn.QueryRow(context.Background(), "SELECT id, title, start_date, end_date, description FROM tb_projects WHERE id = $1", id).Scan(&ProjectDetail.ID, &ProjectDetail.Title, &ProjectDetail.DateStart, &ProjectDetail.DateEnd, &ProjectDetail.Description)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
 	}
+
+	ProjectDetail.Format_date_start = ProjectDetail.DateStart.Format("2 January 2006")
+	ProjectDetail.Format_date_end = ProjectDetail.DateEnd.Format("2 January 2006")
 
 	dataDetail := map[string]interface{}{
 		"Project": ProjectDetail,
@@ -231,9 +228,14 @@ func projectDetail(w http.ResponseWriter, r *http.Request) {
 func deleteProject(w http.ResponseWriter, r *http.Request) {
 	index, _ := strconv.Atoi(mux.Vars(r)["index"])
 
-	projects = append(projects[:index], projects[index+1:]...)
+	_, err := connection.Conn.Exec(context.Background(), "DELETE FROM tb_projects WHERE id = $1", index)
 
-	http.Redirect(w, r, "/", http.StatusFound)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+	}
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
